962bfbf5b04cc568dbb182d050288342da72eb11
Hugo Kooijman <H.V.Kooijman@gmail.com>
2015-06-25 23:41:05 +0200

    added boundingboxes

--- raytracing.cpp
+++ raytracing.cpp
@@ -47,8 +47,8 @@ void init()
 	//MyMesh.loadMesh("cubeonplane.obj", true);
 	//MyMesh.loadMesh("capsule.obj", true);
 	//MyMesh.loadMesh("Rock1.obj", true);
-//	MyMesh.loadMesh("sphereonplane.obj", true);
-//	MyMesh.loadMesh("twospheres.obj", true);
+    //MyMesh.loadMesh("sphereonplane.obj", true);
+    //MyMesh.loadMesh("twospheres.obj", true);
 	//MyMesh.loadMesh("sphereinroomobj.obj", true);
 	MyMesh.loadMesh("cube.obj", true);
 	MyMesh.computeVertexNormals();
@@ -59,6 +59,109 @@ void init()
 	MyLightPositions.push_back(MyCameraPosition);
 }
 
+// Bounding Box vectors
+Vec3Df minvertex, maxvertex;
+
+// Bounding Box class
+class Box {
+    Vec3Df bounds[2];
+public:
+    Box() { }
+    Box(const Vec3Df &min, const Vec3Df &max) {
+        bounds[0] = min;
+        bounds[1] = max;
+    }
+    
+};
+
+Box Boundingbox;
+
+void computeBoundingBoxes()
+{
+    // Test variables for bb
+    float min_x = 999999.f;
+    float min_y = 999999.f;
+    float min_z = 999999.f;
+    
+    float max_x = -999999.f;
+    float max_y = -999999.f;
+    float max_z = -999999.f;
+    
+    for (unsigned int i = 0; i < MyMesh.vertices.size(); i++)
+    {
+        if (MyMesh.vertices[i].p[0] < min_x) {
+            min_x = MyMesh.vertices[i].p[0];
+        }
+        if (MyMesh.vertices[i].p[1] < min_y) {
+            min_y = MyMesh.vertices[i].p[1];
+        }
+        if (MyMesh.vertices[i].p[2] < min_z) {
+            min_z = MyMesh.vertices[i].p[2];
+        }
+        if (MyMesh.vertices[i].p[0] > max_x) {
+            max_x = MyMesh.vertices[i].p[0];
+        }
+        if (MyMesh.vertices[i].p[1] > max_y) {
+            max_y = MyMesh.vertices[i].p[1];
+        }
+        if (MyMesh.vertices[i].p[2] > max_z) {
+            max_z = MyMesh.vertices[i].p[2];
+        }
+    } //http://www.cs.utah.edu/~awilliam/box/
+    
+    minvertex = Vec3Df(min_x, min_y, min_z);
+    maxvertex = Vec3Df(max_x, max_y, max_z);
+    Boundingbox = Box(minvertex, maxvertex);
+}
+
+//Bounding box computed at this moment.
+void computeBoundingBoxes();
+
+//Bounding Box intersection
+bool boxIntersection( const Vec3Df & origin, const Vec3Df & dest) {
+    float tx_min, tx_max, ty_min, ty_max, tz_min, tz_max;
+    
+    //2
+    // Die gast gebruikt by zn t, ty, tzmax "1 - de rest", snap niet waarom.
+    
+    //ray x-coordinate of close intersectionpoint
+    tx_min = ((minvertex.p[0] - origin.p[0]) / dest.p[0]);
+    //ray x-coordinate of far intersectionpoint
+    tx_max = ((maxvertex.p[0] - origin.p[0]) / dest.p[0]);
+    
+    //ray y-coordinate of close intersectionpoint
+    ty_min = ((minvertex.p[1] - origin.p[1]) / dest.p[1]);
+    //ray y-coordinate of far intersectionpoint
+    ty_max = ((maxvertex.p[1] - origin.p[1]) / dest.p[1]);
+    
+    //If true then ray misses
+    if ((tx_min > ty_max) || (ty_min > tx_max)) {
+        return false;
+    }
+    
+    //Find t_in and t_out
+    if (ty_min > tx_min) {
+        tx_min = ty_min;
+    }
+    if (ty_max < tx_max) {
+        tx_max = ty_max;
+    }
+    
+    //ray z-coordinate of close intersectionpoint
+    tz_min = ((minvertex.p[2] - origin.p[2]) / dest.p[2]);
+    //ray z-coordinate of far intersectionpoint
+    tz_max = ((maxvertex.p[2] - origin.p[2]) / dest.p[2]);
+    
+    //If true then ray misses
+    if ((tx_min > tz_max) || (tz_min > tx_max)) {
+        return false;
+    }
+    
+    return true;
+}
+
+
+
 //return the color of your pixel.
 void performRayTracing(const Vec3Df & origin, const Vec3Df & dest, int &level, Vec3Df & color)
 {
@@ -66,6 +169,11 @@ void performRayTracing(const Vec3Df & origin, const Vec3Df & dest, int &level, V
 	// Stop if the maxlevel is reached
 	if(level <= 0)
 		return;
+    
+    //Stop if ray travels outside Bounding Box
+    if( !(boxIntersection(origin, dest))) {
+        return;
+    }
 
 	Vec3Df hit, hitnormal;
 	int triangleIndex = 0;
